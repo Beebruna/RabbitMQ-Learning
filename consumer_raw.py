c91060a13d2683ad57686166ea1edc1557882849
Débora Bruna <beebruna@outlook.com>
2023-07-21 23:26:34 -0300

    implementação do consumidor

---
+++ consumer_raw.py
@@ -0,0 +1,31 @@
+import pika
+
+
+def minha_callback(ch, method, properties, body):
+    print(body)
+    print(type(body))
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port=5672,
+    credentials=pika.PlainCredentials(
+        username="guest",
+        password="guest"
+    )
+)
+
+# Abre canal de conexão com o RabbitMQ
+channel = pika.BlockingConnection(connection_parameters).channel()
+channel.queue_declare(
+    queue="data_queue",
+    durable=True
+)
+
+channel.basic_consume(
+    queue="data_queue",
+    auto_ack=True,
+    on_message_callback=minha_callback #ação condicional
+)
+
+print(f"Listen RabbitMQ on Port 5672")
+channel.start_consuming()
