75553ad76c480a3b9c38bcb17504851e0c918f6b
Débora Bruna <beebruna@outlook.com>
2023-07-22 23:04:47 -0300

    refatoração do publisher

--- publisher.py
+++ publisher.py
@@ -1,22 +1,41 @@
 import pika
+import json
+from typing import Dict
 
 
-connection_parameters = pika.ConnectionParameters(
-    host= "localhost",
-    port=5672,
-    credentials= pika.PlainCredentials(
-        username= "guest",
-        password= "guest"
-    )
-)
+class RabbitmnPublisher:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "guest"
+        self.__password = "guest"
+        self.__exchange = "data_exchange"
+        self.__routing_key = ""
+        self.__channel = self.__create_channel()
 
-channel = pika.BlockingConnection(connection_parameters).channel()
+    def __create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host= self.__host,
+            port=self.__port,
+            credentials= pika.PlainCredentials(
+                username= self.__username,
+                password= self.__password
+            )
+        )
 
-channel.basic_publish(
-    exchange="data_exchange",
-    routing_key="",
-    body="estouMandandoUmaMensagem",
-    properties=pika.BasicProperties(
-        delivery_mode=2 # entrega com persistência
-    )
-)
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+    
+    def send_menssage(self, body: Dict):
+        self.__channel.basic_publish(
+            exchange=self.__exchange,
+            routing_key=self.__routing_key,
+            body=json.dumps(body),
+            properties=pika.BasicProperties(
+                delivery_mode=2 # entrega com persistência
+            )
+        )
+
+    
+rabbitmn_publisher = RabbitmnPublisher()
+rabbitmn_publisher.send_menssage({"ola":"mundo"})
